7d4a17b711b66efc42e3e043c142425cdcf67ebd
Nico <nicopuegher@gmail.com>
2023-08-28 02:00:20 -0300

    Setup nvim-tree default and map custom keys

---
+++ lua/nicolas/plugins/nvim-tree.lua
@@ -0,0 +1,23 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*", -- Latest stable version
+	lazy = false,
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons", lazy = true }, -- Optional plugin
+	},
+	config = function() -- Executed when plugin loads
+		-- Disable Netrw (default file explorer)
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		-- Require and setup nvim-tree
+		local tree = require("nvim-tree") -- Loaded, no need por protected call
+		tree.setup()
+
+		-- Set custom keys
+		local keymap = vim.keymap -- For conciseness
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Find current file" })
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+	end,
+}
