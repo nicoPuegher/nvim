ddaec9480ef3447f5a721fcdbddc31b73e3cb06d
Nico <nicopuegher@gmail.com>
2023-08-29 22:41:52 -0300

    Remove defaults from plugins/colorscheme

--- lua/nicolas/plugins/colorscheme.lua
+++ lua/nicolas/plugins/colorscheme.lua
@@ -1,7 +1,5 @@
 return {
 	"bluz71/vim-nightfly-colors",
-	version = "*", -- Latest stable version
-	lazy = false,
 	priority = 1000,
 	name = "nightfly",
 	config = function() -- Executed when vim-nightfly-colors loads
