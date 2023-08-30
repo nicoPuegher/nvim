7850ce9ef71dab11495fdbb510a00d5cab3baa7a
Nico <nicopuegher@gmail.com>
2023-08-29 22:42:49 -0300

    Remove defaults from lsp/mason

--- lua/nicolas/plugins/lsp/mason.lua
+++ lua/nicolas/plugins/lsp/mason.lua
@@ -1,7 +1,5 @@
 return {
 	"williamboman/mason.nvim",
-	version = "*", -- Latest stable version
-	lazy = false,
 	config = function() -- Executed when mason.nvim loads
 		-- Require and setup mason.nvim
 		local mason = require("mason") -- Loaded, no need for protected call
