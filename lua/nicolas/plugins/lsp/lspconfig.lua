9e44baf18f6fb66ccb2816aae9dde9dea7a710a9
Nico <nicopuegher@gmail.com>
2023-08-31 18:30:05 -0300

    Connect lua-language-server and set it up

--- lua/nicolas/plugins/lsp/lspconfig.lua
+++ lua/nicolas/plugins/lsp/lspconfig.lua
@@ -3,5 +3,8 @@ return {
 	event = { "BufReadPre", "BufNewFile" }, -- Loads on opening or creating a file
 	config = function()
 		local lspconfig = require("lspconfig") -- Loaded, no need for protected call
+
+		-- Connect lua-language-server and set it up
+		lspconfig.lua_ls.setup({})
 	end,
 }
