eea00d7eddf50746696b8b036e7f69f2f7805178
Nico <nicopuegher@gmail.com>
2023-09-01 23:28:46 -0300

    Create an on_attach function that gets called whenever there is an lsp in current buffer

--- lua/nicolas/plugins/lsp/lspconfig.lua
+++ lua/nicolas/plugins/lsp/lspconfig.lua
@@ -4,7 +4,14 @@ return {
 	config = function()
 		local lspconfig = require("lspconfig") -- Loaded, no need for protected call
 
+		-- Create a function that only runs when there is an lsp for the current buffer
+		local on_attach = function(client, bufnr)
+			print("on_attach fired!")
+		end
+
 		-- Connect lua-language-server and set it up
-		lspconfig.lua_ls.setup({})
+		lspconfig.lua_ls.setup({
+			on_attach = on_attach, -- Attaches fn defined above
+		})
 	end,
 }
