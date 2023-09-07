e71d5386d23018e9dabc6fe397bd7348b0a98e66
Nico <nicopuegher@gmail.com>
2023-09-06 23:18:02 -0300

    Add custom keymaps for telescope.nvim

--- lua/nicolas/plugins/telescope.lua
+++ lua/nicolas/plugins/telescope.lua
@@ -5,9 +5,18 @@ return {
 		"nvim-lua/plenary.nvim", -- Required, lib of simplified lua fns
 	},
 	config = function() -- Executed when telescope.nvim loads
-		local telescope = require("telescope") -- Loaded, no need for protected call
+		-- Loaded, no need for protected call
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
 
 		-- Setup telescope.nvim
 		telescope.setup({})
+
+		-- Set custom keys
+		local keymap = vim.keymap -- For conciseness
+		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find a string in cwd" })
 	end,
 }
