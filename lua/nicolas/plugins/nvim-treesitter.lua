7d0aacfc72e958b3049690d015612d85e69c2001
Nico <nicopuegher@gmail.com>
2023-09-07 19:55:44 -0300

    Create nvim-treesitter init file, enable highlights and add lua parser

---
+++ lua/nicolas/plugins/nvim-treesitter.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" }, -- Loads on opening or creating a file
+	build = { ":TSUpdate" }, -- Specified in documentation
+	config = function() -- Executed when nvim-treesitter loads
+		local treesitter = require("nvim-treesitter.configs") -- Loaded, no need for protected call
+
+		-- Setup treesitter
+		treesitter.setup({
+			ensure_installed = { "lua" },
+			highlight = {
+				enable = true,
+			},
+		})
+	end,
+}
