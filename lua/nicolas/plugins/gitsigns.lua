0b5d0b5fb53145522ab275a508e3831697e6067a
Nico <nicopuegher@gmail.com>
2023-09-08 00:06:30 -0300

    Fix comment typos in plugins/bufferline and /gitsigns

--- lua/nicolas/plugins/gitsigns.lua
+++ lua/nicolas/plugins/gitsigns.lua
@@ -1,5 +1,5 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" }, -- Loads on opening or creating a file
-	config = true, -- Substitution for plugin.config(no opts), executes when gitsigns.nvim loads
+	config = true, -- Substitution for plugin.config(no opts), executed when gitsigns.nvim loads
 }
