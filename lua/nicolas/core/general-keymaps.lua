c434b63e50ebd4df85e0a86a7fa557840b0d3ebe
Nico <nicopuegher@gmail.com>
2023-09-09 14:09:18 -0300

    Add window management keymaps in core/general-keymaps

--- lua/nicolas/core/general-keymaps.lua
+++ lua/nicolas/core/general-keymaps.lua
@@ -1,1 +1,9 @@
 vim.g.mapleader = " " -- Set leader key to <Space>
+
+local keymap = vim.keymap -- For conciseness
+
+-- Window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make window splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current window split" })
