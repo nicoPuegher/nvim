4d79c877942e08bcb7d9bd4e3079dbef9a723227
Nico <nicopuegher@gmail.com>
2023-09-12 20:57:43 -0300

    Add keymaps to start and end debugging in dap/nvim-dap

--- lua/nicolas/plugins/dap/nvim-dap.lua
+++ lua/nicolas/plugins/dap/nvim-dap.lua
@@ -1,4 +1,20 @@
 return {
 	"mfussenegger/nvim-dap",
 	lazy = true,
+	keys = { -- Loads on key
+		{
+			"<localleader>db",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			{ desc = "Debugging: Toggle breakpoint" },
+		},
+		{
+			"<localleader>dc",
+			function()
+				require("dap").continue()
+			end,
+			{ desc = "Debugging: Continue" },
+		},
+	},
 }
