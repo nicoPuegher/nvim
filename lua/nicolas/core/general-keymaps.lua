d55355cce591c6d3045d0529e9c214afabd3c836
Nico <nicopuegher@gmail.com>
2023-10-02 22:11:28 -0300

    Add a keymap to clear search highlights in core/keymaps

--- lua/nicolas/core/general-keymaps.lua
+++ lua/nicolas/core/general-keymaps.lua
@@ -13,3 +13,6 @@ keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+
+-- Search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
