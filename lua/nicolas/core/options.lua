458390bf25133fbfbc89a8295d9f2a7772f5a443
Nico <nicopuegher@gmail.com>
2023-10-02 22:12:35 -0300

    Add UI nvim options in core/options

--- lua/nicolas/core/options.lua
+++ lua/nicolas/core/options.lua
@@ -1,5 +1,14 @@
 local opt = vim.opt -- For conciseness
 
+-- UI
+opt.number = true -- Display line numbers
+opt.relativenumber = true -- Display relative line numbers to current line
+opt.signcolumn = "yes" -- Display sign column to avoid content shifting
+opt.wrap = false -- Disable line wrapping
+opt.guicursor = "" -- Use fat cursor always
+opt.cursorline = true -- Highlight current line
+opt.termguicolors = true -- Enable 24-bit RGB color in the terminal
+
 -- Tabs and indentations
 opt.tabstop = 4 -- Make <Tab> 4 character cells wide
 opt.shiftwidth = 4 -- Use <Tab> for indentation
