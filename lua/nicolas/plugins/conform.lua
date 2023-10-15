2cf5b2c94f1a334b59879e42d810a136de79827e
Nicolas <nicopuegher@gmail.com>
2023-10-15 11:15:19 -0300

    Setup conform plugin to format on save and setup lua formatter

--- lua/nicolas/plugins/conform.lua
+++ lua/nicolas/plugins/conform.lua
@@ -1,5 +1,17 @@
 return {
 	'stevearc/conform.nvim',
 	event = { 'BufReadPre', 'BufNewFile' },
-	config = function() end,
+	config = function()
+		local conform = require('conform')
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { 'stylua' },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				timeout_ms = 500,
+			},
+		})
+	end,
 }
