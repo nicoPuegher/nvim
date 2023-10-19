7238882e38fbe47ec1e25b726500b7c155978e0e
Nicolas <nicopuegher@gmail.com>
2023-10-19 14:11:15 -0300

    Setup treesitter in plugins/treesitter

--- lua/nicolas/plugins/treesitter.lua
+++ lua/nicolas/plugins/treesitter.lua
@@ -1,5 +1,26 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	build = ':TSUpdate',
-	config = function() end,
+	config = function()
+		vim.defer_fn(function()
+			require('nvim-treesitter.configs').setup({
+				modules = {
+					'ensure_installed',
+					'auto_install',
+					'sync_install',
+					'ignore_install',
+					'highlight',
+					'indent',
+					'incremental_selection',
+				},
+				ensure_installed = { 'lua' },
+				auto_install = false,
+				sync_install = false,
+				ignore_install = {},
+				highlight = { enable = true, additional_vim_regex_highlighting = false },
+				indent = { enable = true },
+				incremental_selection = { enable = true },
+			})
+		end, 0)
+	end,
 }
