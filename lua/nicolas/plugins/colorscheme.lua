948a32ce72704c8ca15b1a5da2f7a0d8f9363233
Nicolas <nicopuegher@gmail.com>
2023-10-20 14:22:27 -0300

    Setup catppuccin theme in plugins/colorscheme

--- lua/nicolas/plugins/colorscheme.lua
+++ lua/nicolas/plugins/colorscheme.lua
@@ -2,5 +2,13 @@ return {
 	'catppuccin/nvim',
 	name = 'catppuccin',
 	priority = 1000,
-	config = function() end,
+	config = function()
+		require('catppuccin').setup({
+			flavour = 'mocha',
+			background = {
+				dark = 'mocha',
+			},
+		})
+		vim.cmd.colorscheme('catppuccin')
+	end,
 }
