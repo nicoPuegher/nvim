05dc26331e1f59537402fdee4990ac3dbc35eeb9
Nicolas <nicopuegher@gmail.com>
2023-10-21 20:36:59 -0300

    Add user interface options in core/options

--- lua/nicolas/core/options.lua
+++ lua/nicolas/core/options.lua
@@ -1,3 +1,11 @@
+-- User interface
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+vim.opt.guicursor = { 'a:block' }
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+
 -- Tabs and indentation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
