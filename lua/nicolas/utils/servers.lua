7a0a70974f632fd2c955613aa999ad559ea0b528
Nicolas <nicopuegher@gmail.com>
2024-01-17 15:22:47 -0300

    Add love2d library to lua server

--- lua/nicolas/utils/servers.lua
+++ lua/nicolas/utils/servers.lua
@@ -2,9 +2,14 @@ return {
 	servers = {
 		lua_ls = {
 			Lua = {
-				workspace = { checkThirdParty = false },
+				workspace = { checkThirdParty = false, library = {
+					'${3rd}/love2d/library',
+				} },
 				telemetry = { enable = false },
 				hint = { enable = true },
+				library = {
+					'${3rd}/love2d/library',
+				},
 			},
 		},
 	},
