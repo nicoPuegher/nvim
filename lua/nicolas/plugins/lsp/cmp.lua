f509af64d7190d4aa373d71eab8122cfae096564
Nicolas <nicopuegher@gmail.com>
2024-02-20 19:07:35 -0300

    Update nvim-cmp to use the new mappings module

--- lua/nicolas/plugins/lsp/cmp.lua
+++ lua/nicolas/plugins/lsp/cmp.lua
@@ -12,6 +12,7 @@ return {
 		local cmp = require('cmp')
 		local luasnip = require('luasnip')
 		local loaders = require('luasnip.loaders.from_vscode')
+		local mappings = require('nicolas.helper.cmp-mappings').cmp_mappings
 
 		luasnip.config.setup({})
 		loaders.lazy_load()
@@ -23,35 +24,7 @@ return {
 					luasnip.lsp_expand(args.body)
 				end,
 			},
-			mapping = cmp.mapping.preset.insert({
-				['<C-n>'] = cmp.mapping.select_next_item(),
-				['<C-p>'] = cmp.mapping.select_prev_item(),
-				['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-				['<Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-			}),
+			mapping = cmp.mapping.preset.insert(mappings(cmp, luasnip)),
 			sources = {
 				{ name = 'nvim_lsp' },
 				{ name = 'luasnip' },
