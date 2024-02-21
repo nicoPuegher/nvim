ee3cb8624ec8fde4aa60e6757d0add3b52805528
Nicolas <nicopuegher@gmail.com>
2024-02-21 13:29:32 -0300

    Remove unnecessary fallback function in helper/cmp-mappings

--- lua/nicolas/helper/cmp-mappings.lua
+++ lua/nicolas/helper/cmp-mappings.lua
@@ -9,22 +9,18 @@ local function cmp_mappings(cmp, luasnip)
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
-		['<Tab>'] = cmp.mapping(function(fallback) -- Go to next snippet transformation
+		['<Tab>'] = cmp.mapping(function() -- Go to next snippet transformation
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expand_or_locally_jumpable() then
 				luasnip.expand_or_jump()
-			else
-				fallback()
 			end
 		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback) -- Go to previous snippet transformation
+		['<S-Tab>'] = cmp.mapping(function() -- Go to previous snippet transformation
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.locally_jumpable(-1) then
 				luasnip.jump(-1)
-			else
-				fallback()
 			end
 		end, { 'i', 's' }),
 	}
