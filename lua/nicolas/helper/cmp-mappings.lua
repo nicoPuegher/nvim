c18ac30776ce25800004ccf2c5980a98169aa9bf
Nicolas <nicopuegher@gmail.com>
2024-03-21 14:48:59 -0300

    Modify jumpable keys for snippet transformations and confirmation in helper/cmp-mappings

--- lua/nicolas/helper/cmp-mappings.lua
+++ lua/nicolas/helper/cmp-mappings.lua
@@ -5,21 +5,14 @@ local function cmp_mappings(cmp, luasnip)
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		['<Tab>'] = cmp.mapping(function() -- Go to next snippet transformation
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
+		['<C-y>'] = cmp.mapping.confirm({ select = true }),
+		['<C-l>'] = cmp.mapping(function() -- Go to next snippet transformation
+			if luasnip.expand_or_locally_jumpable() then
 				luasnip.expand_or_jump()
 			end
 		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function() -- Go to previous snippet transformation
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
+		['<C-h>'] = cmp.mapping(function() -- Go to previous snippet transformation
+			if luasnip.locally_jumpable(-1) then
 				luasnip.jump(-1)
 			end
 		end, { 'i', 's' }),
