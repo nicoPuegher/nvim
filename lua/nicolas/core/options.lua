7a8074aa371bd9c8d1d7b4ceea4a5232381e4acb
Nicolas <nicopuegher@gmail.com>
2024-03-22 14:24:29 -0300

    Reorder options in core/options

--- lua/nicolas/core/options.lua
+++ lua/nicolas/core/options.lua
@@ -4,26 +4,30 @@ local opt = vim.opt
 -- User interface
 opt.number = true
 opt.relativenumber = true
+opt.scrolloff = 10
 opt.signcolumn = 'yes'
+opt.wrap = false
 opt.guicursor = { 'a:block' }
 opt.cursorline = true
 opt.termguicolors = true
-opt.wrap = false
 
 -- Indentation
 opt.smartindent = true
+opt.breakindent = true
 
 -- Window management
 opt.splitright = true
 opt.splitbelow = true
+
+-- Command line
 opt.inccommand = 'split'
 
 -- Timer
 opt.updatetime = 250
 
--- Miscellaneous
-opt.swapfile = false
-opt.scrolloff = 10
+-- Search
 opt.ignorecase = true
 opt.smartcase = true
-opt.breakindent = true
+
+-- Backup
+opt.swapfile = false
