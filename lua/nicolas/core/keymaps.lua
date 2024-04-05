adb0ce8a2972ec57057b03137bcf747222fb0d5d
Nicolas <nicopuegher@gmail.com>
2024-04-05 10:51:57 -0300

    Add a keymap to move selected lines downwards in core/keymaps

--- lua/nicolas/core/keymaps.lua
+++ lua/nicolas/core/keymaps.lua
@@ -5,3 +5,4 @@ vim.keymap.set('n', '<leader>fe', vim.cmd.Ex, { desc = 'Open [F]ile [E]xplorer' 
 
 -- Text manipulation
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- Move selected line upwards
+vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv") -- Move selected line downwards
