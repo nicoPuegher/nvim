19df687e8e8fe71d1ff3d0875db1e214e8db7988
Nicolas <nicopuegher@gmail.com>
2024-04-05 10:59:23 -0300

    Add a keymap to do a half page jump downwards in core/keymaps

--- lua/nicolas/core/keymaps.lua
+++ lua/nicolas/core/keymaps.lua
@@ -9,3 +9,4 @@ vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv") -- Move selected line downwards
 
 -- Cursor movement
 vim.keymap.set('n', '<C-u', '<C-u>zz') -- Half page jump upwards
+vim.keymap.set('n', '<C-d>', '<C-d>zz') -- Half page jump downwards
