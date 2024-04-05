59bb844695cefae50a65a99403a66b5c1c3c5881
Nicolas <nicopuegher@gmail.com>
2024-04-05 10:49:13 -0300

    Add a keymap to move selected lines upwards in core/keymaps

--- lua/nicolas/core/keymaps.lua
+++ lua/nicolas/core/keymaps.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = ' '
 
 -- File explorer
 vim.keymap.set('n', '<leader>fe', vim.cmd.Ex, { desc = 'Open [F]ile [E]xplorer' })
+
+-- Text manipulation
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- Move selected line upwards
