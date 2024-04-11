9f090d299d977ca5e3659058974590624b6f90f4
Nicolas <nicopuegher@gmail.com>
2024-04-11 13:16:39 -0300

    Add description to all current keymaps

--- lua/nicolas/core/keymaps.lua
+++ lua/nicolas/core/keymaps.lua
@@ -4,20 +4,20 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>fe', vim.cmd.Ex, { desc = 'Open [F]ile [E]xplorer' })
 
 -- Text manipulation
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- Move selected line upwards
-vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv") -- Move selected line downwards
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selected line upwards' })
+vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv", { desc = ' Move selected line downwards' })
 
 -- Cursor movement
-vim.keymap.set('n', '<C-u', '<C-u>zz') -- Half page jump upwards
-vim.keymap.set('n', '<C-d>', '<C-d>zz') -- Half page jump downwards
-vim.keymap.set('n', 'N', 'Nzzzv') -- Upwards in highlight search, keep cursor in the middle
-vim.keymap.set('n', 'n', 'nzzzv') -- Downwards in highlight search, keep cursor in the middle
+vim.keymap.set('n', '<C-u', '<C-u>zz', { desc = ' Half page jump upwards' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Half page jump downwards' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Upwards in highlight search, keep cursor in the middle' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Downwards in highlight search, keep cursor in the middle' })
 
 -- Clipboard
-vim.keymap.set('n', '<leader>Y', '"+Y') -- Yank current line to system clipboard including end of line
-vim.keymap.set('v', '<leader>y', '"+y') -- Yank selected text to system clipboard
-vim.keymap.set('n', '<leader>d', '"*dd') -- Delete current line to system clipboard
-vim.keymap.set('v', '<leader>d', '"*dd') -- Delete selected lines to system clipboard
+vim.keymap.set('n', '<leader>Y', '"+Y', { desc = 'Yank current line to system clipboard including end of line' })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Yank selected text to system clipboard' })
+vim.keymap.set('n', '<leader>d', '"*dd', { desc = 'Delete current line to system clipboard' })
+vim.keymap.set('v', '<leader>d', '"*dd', { desc = 'Delete selected lines to system clipboard' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
