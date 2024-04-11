69b1f4c48f60394303b814473eb8ebabed3abd42
Nicolas <nicopuegher@gmail.com>
2024-04-11 12:41:39 -0300

    Add a keymap to show all diagnostic error messages

--- lua/nicolas/core/keymaps.lua
+++ lua/nicolas/core/keymaps.lua
@@ -22,3 +22,4 @@ vim.keymap.set('v', '<leader>d', '"*dd') -- Delete selected lines to system clip
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
