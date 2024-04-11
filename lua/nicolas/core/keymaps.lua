51c4f28281dc0fd44e56d0cede1b7fc4f7cbd18a
Nicolas <nicopuegher@gmail.com>
2024-04-11 12:40:39 -0300

    Add a keymap to go to next diagnostic message

--- lua/nicolas/core/keymaps.lua
+++ lua/nicolas/core/keymaps.lua
@@ -21,3 +21,4 @@ vim.keymap.set('v', '<leader>d', '"*dd') -- Delete selected lines to system clip
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
