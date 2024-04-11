804ca78858a475b04976f9d504b002b9c739e28f
Nicolas <nicopuegher@gmail.com>
2024-04-11 13:11:11 -0300

    Add a keymap to open the quickfix list

--- lua/nicolas/core/keymaps.lua
+++ lua/nicolas/core/keymaps.lua
@@ -26,3 +26,4 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagn
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setqflist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>zz', { desc = 'Go to next item in the quickfix list' })
 vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>zz', { desc = 'Go to prev item in the quickfix list' })
+vim.keymap.set('n', '<leader>co', '<cmd>copen<CR>', { desc = '[C]ompiler [O]pen (Quickfix list)' })
