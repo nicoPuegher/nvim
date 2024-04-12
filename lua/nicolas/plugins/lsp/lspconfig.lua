e5765f82e52d56f07a61ddfbaf1683612cefc71b
Nicolas <nicopuegher@gmail.com>
2024-04-12 14:48:31 -0300

    Update capabilities in lsp/lspconfig to reflect the new capabilities settings

--- lua/nicolas/plugins/lsp/lspconfig.lua
+++ lua/nicolas/plugins/lsp/lspconfig.lua
@@ -24,8 +24,9 @@ return {
 
 		mason_lspconfig.setup_handlers({
 			function(server_name)
+				local server = utils.servers[server_name] or {}
 				lspconfig[server_name].setup({
-					capabilities = utils.capabilities,
+					capabilities = vim.tbl_deep_extend('force', {}, utils.capabilities, server.capabilities or {}),
 					on_attach = utils.attach_lsp,
 					settings = utils.servers[server_name],
 					filetypes = (utils.servers[server_name] or {}).filetypes,
