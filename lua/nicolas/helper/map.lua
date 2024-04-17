4354f3a344eb35407cd2a8e51ff1ab97bd5ad599
Nicolas <nicopuegher@gmail.com>
2024-04-17 16:15:54 -0300

    Cleanup helper/map

--- lua/nicolas/helper/map.lua
+++ lua/nicolas/helper/map.lua
@@ -1,9 +1,9 @@
 local function map(mode, keys, func, opts, desc)
 	local options = { desc = opts.identifier .. desc }
-
 	if opts.bufnr then
 		options.buffer = opts.bufnr
 	end
+
 	vim.keymap.set(mode, keys, func, options)
 end
 
