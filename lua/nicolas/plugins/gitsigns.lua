d80f61cb0826a0e17811a06b739adacbcdc87223
Nicolas <nicopuegher@gmail.com>
2024-04-17 13:30:17 -0300

    Create a helper function for keymaps and import the gitsigns custom keymaps)

--- lua/nicolas/plugins/gitsigns.lua
+++ lua/nicolas/plugins/gitsigns.lua
@@ -2,9 +2,17 @@ return {
 	'lewis6991/gitsigns.nvim',
 	config = function()
 		local gitsigns = require('gitsigns')
+		local gitsigns_keymaps = require('nicolas.helper.gitsigns_keymaps')
 
 		gitsigns.setup({
 			attach_to_untracked = true,
+			on_attach = function(bufnr)
+				local map = function(mode, keys, func, desc)
+					vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'Gitsigns: ' .. desc })
+				end
+
+				gitsigns_keymaps(map, gitsigns)
+			end,
 		})
 	end,
 }
