e9b9df3636977693faed8de7e9a2db9e2c61a4d4
Nicolas <nicopuegher@gmail.com>
2024-04-20 13:49:09 -0300

    Rename all keymaps for lspconfig_keymaps

--- lua/nicolas/plugins/keymaps/lspconfig_keymaps.lua
+++ lua/nicolas/plugins/keymaps/lspconfig_keymaps.lua
@@ -7,19 +7,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		local buffer = vim.lsp.buf
 		local opts = {
 			bufnr = event.buf,
-			identifier = 'LSP: ',
 		}
 
-		map('n', 'gd', telescope.lsp_definitions, opts, '[G]oto [D]efinition')
-		map('n', 'gr', telescope.lsp_references, opts, '[G]oto [R]eferences')
-		map('n', 'gI', telescope.lsp_implementations, opts, '[G]oto [I]mplementation')
-		map('n', '<leader>D', telescope.lsp_type_definitions, opts, 'Type [D]efinition')
-		map('n', '<leader>ds', telescope.lsp_document_symbols, opts, '[D]ocument [S]ymbols')
-		map('n', '<leader>ws', telescope.lsp_dynamic_workspace_symbols, opts, '[W]orkspace [S]ymbols')
-		map('n', '<leader>rn', buffer.rename, opts, '[R]e[N]ame')
-		map('n', '<leader>ca', buffer.code_action, opts, '[C]ode [A]ction')
-		map('n', 'K', buffer.hover, opts, '[H]over documentation')
-		map('n', 'gD', buffer.declaration, opts, '[G]oto [D]eclaration')
+		map('n', '<leader>ld', telescope.lsp_definitions, opts, '[L]SP [D]efinition')
+		map('n', '<leader>lR', telescope.lsp_references, opts, '[L]SP [R]eferences')
+		map('n', '<leader>li', telescope.lsp_implementations, opts, '[L]SP [I]mplementation')
+		map('n', '<leader>lt', telescope.lsp_type_definitions, opts, '[L]SP [T]ype Definition')
+		map('n', '<leader>ls', telescope.lsp_document_symbols, opts, '[L]SP Document [S]ymbols')
+		map('n', '<leader>lr', buffer.rename, opts, '[L]SP [R]ename')
+		map('n', '<leader>da', buffer.code_action, opts, '[D]iagnostic [A]ctions')
+		map('n', 'K', buffer.hover, opts, 'Hover Documentation')
+		map('n', '<leader>lD', buffer.declaration, opts, '[L]SP [D]eclaration')
 
 		local client = vim.lsp.get_client_by_id(event.data.client_id)
 		if client and client.server_capabilities.documentHighlightProvider then
