f87eeabe8df3808d70b4c81947281b6f009af5b6
Nicolas <nicopuegher@gmail.com>
2024-04-23 15:52:29 -0300

    Import alpha-nvim custom settings and call its function

--- lua/nicolas/plugins/alpha.lua
+++ lua/nicolas/plugins/alpha.lua
@@ -4,7 +4,9 @@ return {
 	config = function()
 		local alpha = require('alpha')
 		local dashboard = require('alpha.themes.dashboard')
+		local alpha_settings = require('nicolas.plugins.settings.alpha_settings')
 
 		alpha.setup(dashboard.opts)
+		alpha_settings(dashboard)
 	end,
 }
