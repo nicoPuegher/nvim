826680b9d9bd326830b50c01ca98f393fb5f3520
Nicolas <nicopuegher@gmail.com>
2024-04-29 16:41:21 -0300

    Add key register to the key_settings file for which-key.nvim

--- lua/nicolas/plugins/settings/key_settings.lua
+++ lua/nicolas/plugins/settings/key_settings.lua
@@ -23,4 +23,23 @@ local function key_settings()
 	}
 end
 
-return key_settings
+local function key_register()
+	return {
+		['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+		['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+		['<leader>d'] = { name = '[D]iagnostics', _ = 'which_key_ignore' },
+		['<leader>l'] = { name = '[L]SP', _ = 'which_key_ignore' },
+		['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+		['<leader>n'] = { name = '[N]otes', _ = 'which_key_ignore' },
+		['<leader>q'] = { name = '[Q]uickfix', _ = 'which_key_ignore' },
+		['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
+		['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+		['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
+		['<leader>sg'] = { name = '[G]it', _ = 'which_key_ignore' },
+	}
+end
+
+return {
+	key_settings = key_settings,
+	key_register = key_register,
+}
