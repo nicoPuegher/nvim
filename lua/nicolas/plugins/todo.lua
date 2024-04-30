1f75d38a0eec2208ad1f8a9d1471f6cfe8a52a37
Nicolas <nicopuegher@gmail.com>
2024-04-30 17:07:42 -0300

    Reflect changes for todo-comments.nvim

--- lua/nicolas/plugins/todo.lua
+++ lua/nicolas/plugins/todo.lua
@@ -4,12 +4,10 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		local todo = require('todo-comments')
+		local todo_settings = require('nicolas.plugins.settings.todo_settings')
 		local todo_keymaps = require('nicolas.plugins.keymaps.todo_keymaps')
 
-		todo.setup({
-			signs = false,
-		})
-
+		todo.setup(todo_settings())
 		todo_keymaps()
 	end,
 }
