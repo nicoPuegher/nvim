d2bd46391451a45ed85ca1c34332ce50e5451293
Nicolas <nicopuegher@gmail.com>
2024-05-03 12:01:41 -0300

    Modify lazy loading event for plugins

--- lua/nicolas/plugins/gitsigns.lua
+++ lua/nicolas/plugins/gitsigns.lua
@@ -1,5 +1,6 @@
 return {
 	'lewis6991/gitsigns.nvim',
+	event = { 'BufReadPre', 'BufNewFile' },
 	config = function()
 		local gitsigns = require('gitsigns')
 		local gitsigns_settings = require('nicolas.plugins.settings.gitsigns_settings')
