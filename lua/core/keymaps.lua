2ef3300b146685bac9988b0273be0089106bd4f2
Nicolas <nicopuegher@gmail.com>
2024-09-25 23:30:40 -0300

    Add a keymap to yank selection to the clipboard in core/keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,3 +4,6 @@ vim.g.maplocalleader = '\\'
 
 -- Explorer
 vim.keymap.set('n', '<leader>fe', vim.cmd.Ex, { desc = '[F]ile [E]xplorer' })
+
+-- Clipboard
+vim.keymap.set('v', '<leader>y', '"+y', { desc = '[Y]ank selection to clipboard' })
