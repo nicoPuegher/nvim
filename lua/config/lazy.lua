d1243ebe4a3e4e3aac4c997d23c7af4ffc71662e
Nicolas <nicopuegher@gmail.com>
2024-09-27 10:42:33 -0300

    Disable luarocks for lazy.nvim in config/lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,4 +21,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
     spec = {},
     checker = { enabled = true },
+    rocks = {
+        hererocks = false,
+        enabled = false,
+    },
 })
