002ab4f6b9e4835e4fa2c150594a41a7ce751dcd
Nicolas <nicopuegher@gmail.com>
2024-10-01 16:04:19 -0300

    Import cmp_luasnip as a dependency for nvim-cmp in /plugins/cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,5 +2,6 @@ return {
     'hrsh7th/nvim-cmp',
         dependencies = {
         { 'L3MON4D3/LuaSnip', version = 'v2.*', build = 'make install_jsregexp' },
+        'saadparwaiz1/cmp_luasnip',
     },
 }
