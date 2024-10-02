dd1f152f6309bbf57eb21d6f42403fefbd88c4bd
Nicolas <nicopuegher@gmail.com>
2024-10-02 16:09:37 -0300

    Import cmp-nvim-lsp as a dependency for nvim-cmp in /plugins/cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,5 +3,6 @@ return {
         dependencies = {
         { 'L3MON4D3/LuaSnip', version = 'v2.*', build = 'make install_jsregexp' },
         'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lsp',
     },
 }
