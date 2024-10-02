2ebd65f4cde615cfd751e96744c12c924b604d73
Nicolas <nicopuegher@gmail.com>
2024-10-02 16:12:13 -0300

    Import cmp-buffer as a dependency for nvim-cmp in /plugins/cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,5 +4,6 @@ return {
         { 'L3MON4D3/LuaSnip', version = 'v2.*', build = 'make install_jsregexp' },
         'saadparwaiz1/cmp_luasnip',
         'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
     },
 }
