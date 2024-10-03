33c0c0531d8008813cf210fa232d419c77082ef8
Nicolas <nicopuegher@gmail.com>
2024-10-03 16:32:25 -0300

    Set a custom configuration for the sources option in /plugins/cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,6 +19,12 @@ return {
                     luasnip.lsp_expand(args.body)
                 end,
             },
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'buffer' },
+                { name = 'path' },
+            }),
         })
     end,
 }
