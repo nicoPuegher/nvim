47a23f6ffd60bf4ce8a04be1263c7bb79337bebe
Nicolas <nicopuegher@gmail.com>
2024-10-04 17:47:12 -0300

    Set custom keymaps to jump between snippet properties for the mappings option in /plugins/cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -32,6 +32,16 @@ return {
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
                 ['<C-e>'] = cmp.mapping.abort(),
                 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-l>'] = cmp.mapping(function()
+                    if luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    end
+                end, { 'i', 's' }),
+                ['<C-h>'] = cmp.mapping(function()
+                    if luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    end
+                end, { 'i', 's' }),
             },
         })
     end,
