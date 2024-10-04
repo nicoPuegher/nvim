4076e36a0d32b5532b55cadd878d963f2c4f0886
Nicolas <nicopuegher@gmail.com>
2024-10-04 17:44:06 -0300

    Set custom keymaps to scroll backward and forward for the mappings option in /plugins/cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -28,6 +28,8 @@ return {
             mapping = {
                 ['<C-n>'] = cmp.mapping.select_next_item(),
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
             },
         })
     end,
