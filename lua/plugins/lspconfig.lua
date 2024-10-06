307fb83ccaf9f757552a7235f923e89effa60dcf
Nicolas <nicopuegher@gmail.com>
2024-10-06 14:12:51 -0300

    Create capabilities for the servers in /plugins/lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -15,5 +15,8 @@ return {
                 },
             },
         }
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
     end
 }
