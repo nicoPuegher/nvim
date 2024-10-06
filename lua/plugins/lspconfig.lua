f69616931a86e00cf339966d51f9b1c1925fba98
Nicolas <nicopuegher@gmail.com>
2024-10-06 14:11:47 -0300

    Create a table for the servers and configure lua_ls in /plugins/lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,4 +3,17 @@ return {
     dependencies = {
         { 'williamboman/mason-lspconfig.nvim', opts = {} },
     },
+    config = function()
+        local servers = {
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        completion = {
+                            callSnippet = 'Replace',
+                        },
+                    },
+                },
+            },
+        }
+    end
 }
