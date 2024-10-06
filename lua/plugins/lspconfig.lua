d9e6a0cf977fdc4ae23bf7c61be9ddcfbe03e1bf
Nicolas <nicopuegher@gmail.com>
2024-10-06 14:14:05 -0300

    Setup mason-lspconfig by passing custom capabilities to the selected servers in /plugins/lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -18,5 +18,15 @@ return {
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+        require('mason-lspconfig').setup {
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                    require('lspconfig')[server_name].setup(server)
+                end,
+            }
+        }
     end
 }
