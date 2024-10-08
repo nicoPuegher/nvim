c463a602b4b2314568dbd891d7da53802cf36b23
Nicolas <nicopuegher@gmail.com>
2024-10-08 12:41:33 -0300

    Set format on save to disable filetypes or use fallback for the format_on_save option in /plugins/conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,4 +1,19 @@
 return {
     'stevearc/conform.nvim',
-    opts = {},
+    opts = {
+        format_on_save = function(bufnr)
+            local disable_filetypes = {}
+            local lsp_format_opt = ''
+
+            if disable_filetypes[vim.bo[bufnr].filetype] then
+                lsp_format_opt = 'never'
+            else
+                lsp_format_opt = 'fallback'
+            end
+            return {
+                timeout_ms = 500,
+                lsp_format = lsp_format_opt,
+            }
+        end,
+    },
 }
