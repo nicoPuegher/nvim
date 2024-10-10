20d6a6f7720d9a2aa7bb7654d995a4095736a5b6
Nicolas <nicopuegher@gmail.com>
2024-10-10 15:31:17 -0300

    Set CmdlineEnter and InsertEnter as load events for nvim-cmp in /plugins/cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,5 +1,6 @@
 return {
     'hrsh7th/nvim-cmp',
+    event = { 'CmdlineEnter', 'InsertEnter' },
     dependencies = {
         { 'L3MON4D3/LuaSnip', version = 'v2.*', build = 'make install_jsregexp' },
         'saadparwaiz1/cmp_luasnip',
