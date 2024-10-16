ed526e349e0170832a51161de76ca4d947ff913f
Nicolas <nicopuegher@gmail.com>
2024-10-16 15:18:19 -0300

    Set cond spec loading to true when 'make' exists for telescope-fzf-native.nvim dependency in /plugins/telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,9 @@ return {
         {
             'nvim-telescope/telescope-fzf-native.nvim',
             build = 'make',
+            cond = function()
+                return vim.fn.executable('make') == 1
+            end,
         },
     },
 }
