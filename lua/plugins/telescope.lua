886c5218c3bf424ec3476558425bdc402b21db9a
Nicolas <nicopuegher@gmail.com>
2024-10-19 15:53:52 -0300

    Set help_tags keymap for telescope.nvim in /plugins/telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,5 +38,6 @@ return {
         pcall(telescope.load_extension, 'ui-select')
 
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     end,
 }
