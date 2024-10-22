8c39e129a7e03644e3ef59ca5c93b571c632e7df
Nicolas <nicopuegher@gmail.com>
2024-10-22 16:12:44 -0300

    Set neovim find_files for telescope.nvim in /plugins/telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -57,5 +57,8 @@ return {
         vim.keymap.set('n', '<leader>si', function()
             builtin.live_grep({ grep_open_files = true })
         end, { desc = '[S]earch [I]n open files' })
+        vim.keymap.set('n', '<leader>sn', function()
+            builtin.find_files({ cwd = vim.fn.stdpath('config') })
+        end, { desc = '[S]earch [N]eovim files' })
     end,
 }
