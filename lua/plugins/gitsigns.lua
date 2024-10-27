c4b8cdf0ad1d7a9dc62e0c0663f7e0cbdcbc1743
Nicolas <nicopuegher@gmail.com>
2024-10-27 16:54:32 -0300

    Set nav_hunk('prev') keymap for gitsigns.nvim in /plugins/gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,6 +5,14 @@ return {
         numhl = true,
         on_attach = function(bufnr)
             local gitsigns = require('gitsigns')
+
+            vim.keymap.set('n', '[g', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ '[g', bang = true })
+                else
+                    gitsigns.nav_hunk('prev')
+                end
+            end, { desc = 'Jump to the previous [G]it change', buffer = bufnr })
         end,
     },
 }
