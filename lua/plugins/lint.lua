7c32993d334bd9d1972e77a0b7795f2c283ee82b
Nicolas <nicopuegher@gmail.com>
2024-10-30 22:01:39 -0300

    Create an autocmd for nvim-lint to automatically lint files in /plugins/lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -7,5 +7,15 @@ return {
         lint.linters_by_ft = {
             lua = { 'selene' },
         }
+
+        local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
+            group = lint_augroup,
+            callback = function()
+                if vim.opt_local.modifiable:get() then
+                    lint.try_lint()
+                end
+            end,
+        })
     end,
 }
