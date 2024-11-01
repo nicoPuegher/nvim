62fa32ff33fcf926a1d25cb10fad6192c90ac2f8
Nicolas <nicopuegher@gmail.com>
2024-10-31 23:06:10 -0300

    Attach a diagnostics configuration for nvim-lspconfig in /plugins/lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -32,6 +32,9 @@ return {
                 function(server_name)
                     local server = servers[server_name] or {}
                     server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                    server.on_attach = function()
+                        vim.diagnostic.config({})
+                    end
                     lspconfig[server_name].setup(server)
                 end,
             },
