e1114e6890237ff821ccd1957655551ec131928c
Nicolas <nicopuegher@gmail.com>
2024-11-02 18:05:09 -0300

    Add dependencies needed for lualine.nvim in /plugins/lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,9 @@
 return {
     'nvim-lualine/lualine.nvim',
     event = 'VeryLazy',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        'mfussenegger/nvim-lint',
+        'stevearc/conform.nvim',
+    },
 }
