0b826816ade1e2e18d2640aa8d47a112bc1b125e
Nicolas <nicopuegher@gmail.com>
2024-11-01 23:39:58 -0300

    Add group keymap descriptions for current custom keymaps for which-key.nvim in /plugins/key

--- lua/plugins/key.lua
+++ lua/plugins/key.lua
@@ -9,5 +9,13 @@ return {
         icons = {
             mappings = false,
         },
+        spec = {
+            { '<leader>s', group = '[S]earch' },
+            { '<leader>c', group = '[C]ode' },
+            { '<leader>g', group = '[G]it' },
+            { '<leader>f', group = '[F]ile' },
+            { '<leader>sg', group = '[G]it' },
+            { '<leader>gt', group = '[T]oggle' },
+        },
     },
 }
