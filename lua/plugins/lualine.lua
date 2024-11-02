b9bde676c584289722c6a322e62a076e8f35af5c
Nicolas <nicopuegher@gmail.com>
2024-11-02 18:09:33 -0300

    Create local variables for lualine.nvim clients in /plugins/lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,6 +9,10 @@ return {
     opts = function()
         local lualine = require('lualine')
 
-        local function get_attached_clients() end
+        local function get_attached_clients()
+            local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
+            local buf_ft = vim.bo.filetype
+            local components = {}
+        end
     end,
 }
