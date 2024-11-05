5387b70e3e4db55f8101378220208c70887d0162
Nicolas <nicopuegher@gmail.com>
2024-11-04 21:33:16 -0300

    Set keys spec lazy loading to a custom keymap for undotree in /plugins/undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,4 +1,14 @@
 return {
     'mbbill/undotree',
     event = 'VeryLazy',
+    keys = {
+        {
+            '<leader>tu',
+            function()
+                vim.cmd.UndotreeToggle()
+            end,
+            mode = 'n',
+            desc = '[T]oggle [U]ndotree',
+        },
+    },
 }
