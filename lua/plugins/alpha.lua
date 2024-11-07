982aec4afa0a8bd253ff82a136d2d675e5c21ca1
Nicolas <nicopuegher@gmail.com>
2024-11-06 22:20:15 -0300

    Require alpha, alpha.themes.dashboard and alpha fortune and assign them to a local variable for conciseness in /plugins/alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,5 +1,9 @@
 return {
     'goolord/alpha-nvim',
     event = 'VimEnter',
-    config = function() end,
+    config = function()
+        local alpha = require('alpha')
+        local dashboard = require('alpha.themes.dashboard')
+        local fortune = require('alpha.fortune')
+    end,
 }
