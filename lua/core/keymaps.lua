8a6a657a8d6badd7b8665b251b3880bb25cf959c
Nicolas <nicopuegher@gmail.com>
2024-11-07 20:34:41 -0300

    Create a custom keymap to move selection down in /core/keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,5 +5,8 @@ vim.g.maplocalleader = '\\'
 -- Explorer
 vim.keymap.set('n', '<leader>fe', vim.cmd.Ex, { desc = '[F]ile [E]xplorer' })
 
+-- Text manipulation
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+
 -- Clipboard
 vim.keymap.set('v', '<leader>y', '"+y', { desc = '[Y]ank selection to clipboard' })
