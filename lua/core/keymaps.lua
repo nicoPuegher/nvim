2bddd79245aa8fbbc240a98cb70fd734f0bf7b88
Nicolas <nicopuegher@gmail.com>
2024-11-07 23:16:10 -0300

    Create a custom keymap to replace text under the cursor globally in /core/keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,6 +9,12 @@ vim.keymap.set('n', '<leader>fe', vim.cmd.Ex, { desc = '[F]ile [E]xplorer' })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
 vim.keymap.set('v', '<leader>p', '"_dP', { desc = '[P]aste keep copy' })
+vim.keymap.set(
+    'n',
+    '<leader>r',
+    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = '[R]eplace under cursor' }
+)
 
 -- Clipboard
 vim.keymap.set('v', '<leader>y', '"+y', { desc = '[Y]ank selection to clipboard' })
