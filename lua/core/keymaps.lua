6ca5bc45e1441323a732c3c6383789ab5b7166d9
Nicolas <nicopuegher@gmail.com>
2024-11-07 23:14:45 -0300

    Create a custom keymap to paste selection over text and not lose the copy in /core/keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,6 +8,7 @@ vim.keymap.set('n', '<leader>fe', vim.cmd.Ex, { desc = '[F]ile [E]xplorer' })
 -- Text manipulation
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+vim.keymap.set('v', '<leader>p', '"_dP', { desc = '[P]aste keep copy' })
 
 -- Clipboard
 vim.keymap.set('v', '<leader>y', '"+y', { desc = '[Y]ank selection to clipboard' })
