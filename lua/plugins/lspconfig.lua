8dfcdd033d61b745635a92c7b8d708abb84a5200
Nicolas <nicopuegher@gmail.com>
2024-11-09 21:34:13 -0300

    Create a custom keymap to enable inlay hints if the server allows it in /plugins/lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -68,6 +68,12 @@ return {
                         end,
                     })
                 end
+
+                if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+                    map('<leader>th', function()
+                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+                    end, '[T]oggle [H]ints')
+                end
             end,
         })
 
