0bdf849f5deab259a8108327877c322bf0d7dc28
Nicolas <nicopuegher@gmail.com>
2024-11-10 18:47:52 -0300

    Setup different linters in /plugins/lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -6,6 +6,10 @@ return {
 
         lint.linters_by_ft = {
             lua = { 'selene' },
+            html = { 'htmlhint' },
+            css = { 'stylelint' },
+            javascript = { 'eslint_d' },
+            javascriptreact = { 'eslint_d' },
         }
 
         local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
