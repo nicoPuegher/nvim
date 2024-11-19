fc237c543e675e96878c5dbf2647c9b53516ae73
Nicolas <nicopuegher@gmail.com>
2024-11-19 20:59:05 -0300

    Require nvim-ufo and assign it to a local variable for conciseness in /plugins/ufo

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -2,5 +2,7 @@ return {
     'kevinhwang91/nvim-ufo',
     event = 'VeryLazy',
     dependencies = 'kevinhwang91/promise-async',
-    config = function() end,
+    config = function()
+        local ufo = require('ufo')
+    end,
 }
