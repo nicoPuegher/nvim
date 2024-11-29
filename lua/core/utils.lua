7eeb2e8b02d043c10b97ddb9d001a8ed2b3edb39
Nicolas <nicopuegher@gmail.com>
2024-11-29 12:33:39 -0300

    Add a custom command in /utils to restart prettierd if needed

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -5,3 +5,8 @@ vim.filetype.add({
         ['.env'] = 'sh',
     },
 })
+
+-- Commands
+vim.api.nvim_create_user_command('PrettierdRestart', function()
+    os.execute('pkill -f prettierd')
+end, {})
