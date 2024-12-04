a6073b99fc187f05b21bc1a80790a4eb45185d92
Nicolas <nicopuegher@gmail.com>
2024-12-04 13:14:32 -0300

    Disable htmlint and stylelint linters

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -6,8 +6,8 @@ return {
 
         lint.linters_by_ft = {
             lua = { 'selene' },
-            html = { 'htmlhint' },
-            css = { 'stylelint' },
+            -- html = { 'htmlhint' },
+            -- css = { 'stylelint' },
             javascript = { 'eslint_d' },
             javascriptreact = { 'eslint_d' },
         }
