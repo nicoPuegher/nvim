690b64e7ec7a9b1bf1a085be682705bc4b26c154
Nicolas <nicopuegher@gmail.com>
2024-12-05 13:15:37 -0300

    Change event spec lazy loading for nvim-lint from 'BufReadPre' to 'BufRead'

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,6 +1,6 @@
 return {
     'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
+    event = { 'BufRead', 'BufNewFile' },
     config = function()
         local lint = require('lint')
 
