694d06c91ad6d93fcc257ad6a33dc692eecc2609
Nicolas <nicopuegher@gmail.com>
2024-12-07 13:28:30 -0300

    Use nvim-web-devicons as a spec for mini.icons

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -2,4 +2,7 @@ return {
     'echasnovski/mini.icons',
     event = 'VimEnter',
     opts = {},
+    specs = {
+        { 'nvim-tree/nvim-web-devicons', enabled = false, optional = true },
+    },
 }
