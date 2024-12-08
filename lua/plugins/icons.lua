b6d509e809b9e54e570e3ada8dd7ed601dacd34a
Nicolas <nicopuegher@gmail.com>
2024-12-08 13:29:13 -0300

    Set an init function for mini.icons to mock nvim-web-devicons

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -5,4 +5,10 @@ return {
     specs = {
         { 'nvim-tree/nvim-web-devicons', enabled = false, optional = true },
     },
+    init = function()
+        package.preload['nvim-web-devicons'] = function()
+            require('mini.icons').mock_nvim_web_devicons()
+            return package.loaded['nvim-web-devicons']
+        end
+    end,
 }
