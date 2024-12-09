1fba89109f5f58e7f1d0ca45ffb0769276adb3b0
Nicolas <nicopuegher@gmail.com>
2024-12-09 13:34:38 -0300

    Change the spec setup for nvim-ufo from 'config' to 'opts'

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -2,9 +2,5 @@ return {
     'kevinhwang91/nvim-ufo',
     event = 'InsertEnter',
     dependencies = 'kevinhwang91/promise-async',
-    config = function()
-        local ufo = require('ufo')
-
-        ufo.setup()
-    end,
+    opts = {},
 }
