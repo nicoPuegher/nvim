6dfbc8ef5f5bf10359d5d037a547e16de5e3e5de
Nicolas <nicopuegher@gmail.com>
2024-12-14 14:26:59 -0300

    Add custom todo-comments.nvim keymaps to move between todo comments

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -8,5 +8,13 @@ return {
         todo.setup({
             signs = false,
         })
+
+        vim.keymap.set('n', ']t', function()
+            todo.jump_next()
+        end, { desc = 'Next [T]odo comment' })
+
+        vim.keymap.set('n', '[t', function()
+            todo.jump_prev()
+        end, { desc = 'Previous [T]odo comment' })
     end,
 }
