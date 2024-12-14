b19258a266d3683ad61fdea0b1c0fcfbfbcbbf94
Nicolas <nicopuegher@gmail.com>
2024-12-14 14:26:33 -0300

    Setup todo-comments.nvim

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -2,4 +2,11 @@ return {
     'folke/todo-comments.nvim',
     event = 'InsertEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local todo = require('todo-comments')
+
+        todo.setup({
+            signs = false,
+        })
+    end,
 }
