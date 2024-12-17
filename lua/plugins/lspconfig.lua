c3ff0006573a7a8f09febb12a8486b9516945564
Nicolas <nicopuegher@gmail.com>
2024-12-17 15:16:56 -0300

    Setup mason.nvim using opts spec setup instead of config to load it first

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,8 +1,7 @@
 return {
     'neovim/nvim-lspconfig',
-    -- event = 'BufRead',
     dependencies = {
-        { 'williamboman/mason.nvim', config = true },
+        { 'williamboman/mason.nvim', opts = {} },
         { 'williamboman/mason-lspconfig.nvim' },
         { 'j-hui/fidget.nvim', opts = {} },
         'hrsh7th/cmp-nvim-lsp',
