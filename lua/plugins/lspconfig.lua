948da0fa5fd6c072f33e4b5543cb584d9aac775d
Nicolas <nicopuegher@gmail.com>
2024-12-21 17:22:34 -0300

    Remove sign items from lsp diagnostics for nvim-lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -129,12 +129,6 @@ return {
             end,
         })
 
-        local signs = { Error = '', Warn = '', Hint = '', Info = '' }
-        for type, icon in pairs(signs) do
-            local hl = 'DiagnosticSign' .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
-
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend('force', capabilities, cmp_nvim_lsp.default_capabilities())
         capabilities.textDocument.foldingRange = {
