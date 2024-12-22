bb02e10d5c405984ceec259f0d0341b534700924
Nicolas <nicopuegher@gmail.com>
2024-12-22 17:30:19 -0300

    Add lspkind.nvim as a dependency for nvim-cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,6 +8,7 @@ return {
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
+        'onsails/lspkind.nvim',
     },
     opts = function()
         local luasnip = require('luasnip')
