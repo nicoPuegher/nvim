eaac47268d0097bb7a15daeaee17dedd1b787213
Nicolas <nicopuegher@gmail.com>
2024-12-24 17:33:17 -0300

    Remove cmp-path source for nvim-cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -27,7 +27,6 @@ return {
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' },
-                { name = 'path' },
             }, {
                 { name = 'buffer' },
             }),
