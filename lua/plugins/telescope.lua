b80c2cc97f5f9f3bcdbfaaeb24bc2bd9c3391c8f
Nicolas <nicopuegher@gmail.com>
2024-12-25 17:45:52 -0300

    Add plenary as a dependency for telescope.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
     branch = '0.1.x',
     event = 'VimEnter',
     dependencies = {
+        'nvim-lua/plenary.nvim',
         {
             'nvim-telescope/telescope-fzf-native.nvim',
             build = 'make',
